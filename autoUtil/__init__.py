550bb7c3525798f4af0caa09714ea1637ff9610d
jeb <wangqiuyue2016@gmail.com>
2018-12-14 12:38:49 +0800

    delete all code

--- autoUtil/__init__.py
+++
@@ -1,4 +0,0 @@
-# autoUtil __init__.py
-
-from ssh_interface import *
-from auto_getflag import auto_get_submit
--- submit/192.168.43.243/192.168.43.243.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-while true; do
-    python /Users/jeb/Desktop/AutoBinForAWD/submit/192.168.43.243/192.168.43.243.py >> /Users/jeb/Desktop/AutoBinForAWD/submit/192.168.43.243/log.txt
-    sleep 5s
-done
-    
