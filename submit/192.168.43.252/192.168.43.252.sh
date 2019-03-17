748c7193daca283d1a5f354e916d05204fe67c56
jeb <wangqiuyue2016@xgmailcom>
2019-03-17 20:17:43 +0800

    update

---
+++ submit/192.168.43.252/192.168.43.252.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+while true; do
+    python /Users/jeb/Desktop/AutoBinForAWD/submit/192.168.43.252/192.168.43.252.py >> /Users/jeb/Desktop/AutoBinForAWD/submit/192.168.43.252/log.txt
+    sleep 5s
+done
+    
